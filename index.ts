bfd2d065a31f8510ce4145f9f4926eec822703ac
TheWander02 <48934424+thewander02@users.noreply.github.com>
2025-08-10 19:53:31 -0700

    new sdk

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { createClient } from '@hey-api/openapi-ts';
+
+createClient({
+  input: 'https://api.blckbox.dev/api-docs/openapi.json',
+  output: 'src/client',
+  plugins: [
+    '@hey-api/typescript',
+    '@hey-api/client-next',
+    '@tanstack/react-query', 
+  ],
+});
