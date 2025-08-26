5e070fb7df0964d14b360b36a925030ab3f991a2
TheWander02 <48934424+thewander02@users.noreply.github.com>
2025-08-25 19:17:13 -0700

    chore: update spec url

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@hey-api/openapi-ts';
 
 createClient({
-  input: 'https://api.blckbox.dev/api-docs/openapi.json',
+  input: 'https://api.blckbox.dev/api/v1/openapi.json',
   output: 'src/client',
   plugins: [
     '@hey-api/typescript',
